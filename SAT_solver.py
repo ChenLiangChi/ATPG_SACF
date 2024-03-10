460380264a1d356ddd591d7fb7cdb0f8219e852b
Liang Chi Chen <liangchichen@liangdembp.speedport.ip>
2024-03-10 19:20:40 +0100

    Add CNF conversion functions

--- SAT_solver.py
+++ SAT_solver.py
@@ -16,11 +16,24 @@ def main():
     if equal_constraints:
         label_equal_constraints = label_to_int(equal_constraints)
         equal_constraints_cnf = equation_cnf(label_equal_constraints)
-        print(equal_constraints_cnf)
+        print("The CNF constraints of equality is: ",equal_constraints_cnf.clauses)
     if addition_constraints:
         label_addition_constraints = label_to_int(addition_constraints)
-        addition_constraints_cnf = equation_cnf(label_addition_constraints)
-        print(addition_constraints_cnf)
+        addition_constraints_cnf = equation_01_cnf(label_addition_constraints)
+        print("The CNF constraints of equality is: ",addition_constraints_cnf.clauses)
+    if xor_constraints_faulty:
+        label_xor_constraints_faulty = label_to_int(xor_constraints_faulty)
+        xor_constraints_faulty_cnf = xor_to_cnf(label_xor_constraints_faulty)
+        print(label_xor_constraints_faulty)
+        print(xor_constraints_faulty_cnf.clauses)
+    if fault_active_constraints_control:
+        label_fault_active_constraints_control = label_to_int(fault_active_constraints_control)
+        fault_active_constraints_control_cnf = equation_01_cnf(label_fault_active_constraints_control)
+        print(fault_active_constraints_control_cnf.clauses)
+    if fault_active_constraints_faulty:
+        label_fault_active_constraints_faulty = label_to_int(fault_active_constraints_faulty)
+        fault_active_constraints_faulty_cnf = equation_01_cnf(label_fault_active_constraints_faulty)
+        print(fault_active_constraints_faulty_cnf.clauses)
         
     print(label_map)
     
@@ -44,6 +57,36 @@ def equation_cnf(constraints):
         equ_cnf.append([-constraints[idx][0], constraints[idx][1]])
     #print(equ_cnf.clauses)
     return equ_cnf
+
+def equation_01_cnf(constraints):
+    equ_01_cnf = CNF()
+    key_is_1 = label_map.get(1)
+    key_is_0 = label_map.get(0)
+    for constraint in constraints:
+        for label in constraint:
+            if label == key_is_1:
+                equ_01_cnf.append([constraint[0]])  
+            elif label == key_is_0:
+                equ_01_cnf.append([-constraint[0]])  
+    return equ_01_cnf
+
+def xor_to_cnf(constraints):
+    xor_cnf = CNF()
+    for constraint in constraints:
+        a, b = constraint[:2]
+        conjuncts = constraint[2:]
+        xor_cnf.append([a, b])
+        xor_cnf.append([-a, -b])
+        for conjunct in conjuncts:
+            xor_cnf.append([a, -b, conjunct])
+            xor_cnf.append([-a, b, conjunct])
+            for clause in xor_cnf.clauses:
+                if clause[:2] == [a, b]:
+                    clause.extend([-conjunct])
+                elif clause[:2] == [-a, -b]:
+                    clause.extend([-conjunct])
+        print(conjuncts)
+    return xor_cnf
 
 if __name__ == '__main__': 
     label_map = {}
